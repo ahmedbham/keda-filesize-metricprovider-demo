b571a6cbe60ca6eb7616c362b5fe1bf8d9b21cf5
Ahmed Bham <ahmed.bham@gmail.com>
2021-12-20 18:59:46 +0000

    java project created

---
+++ keda-filesize-metricprovider-demo/src/main/java/com/example/kedafilesizemetricproviderdemo/KedaFilesizeMetricproviderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kedafilesizemetricproviderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedaFilesizeMetricproviderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KedaFilesizeMetricproviderDemoApplication.class, args);
+	}
+
+}
