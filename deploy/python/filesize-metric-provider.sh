dab31999a7e097064ab093860bd7a7fd752921bc
Ahmed Bham <ahmed.bham@gmail.com>
2021-12-28 09:41:05 +0000

    created python metric provider api app

---
+++ deploy/python/filesize-metric-provider.sh
@@ -0,0 +1,22 @@
+cd keda-filesize-metricprovider-app
+
+pip install -r requirements.txt
+
+echo "Pushing filesize-metric-provider image to ACR"
+acrLoginServer="dapr1batch.azurecr.io"
+acrName="dapr1batch"
+
+docker build -t $acrLoginServer/filesize-metric-provider:v1 .
+
+# Log in to the registry
+az acr login --name $acrName
+
+# Push the image to the Azure Container Registry instance
+docker push $acrLoginServer/filesize-metric-provider:v1
+
+cd ..
+
+# Deploy to AKS cluster
+kubectl delete -f deploy/python/filesize-metric-provider.yaml
+sleep 10
+kubectl apply -f deploy/python/filesize-metric-provider.yaml
